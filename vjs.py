8c0caabd30cd11eef66d138087c5ba34fdd32c1c
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-15 22:31:08 -0700

    Create vjs.py

---
+++ vjs.py
@@ -0,0 +1,8 @@
+string = "geeks for geeks" 
+  
+# counts the number of times substring occurs in  
+# the given string between index 0 and 5 and returns  
+# an integer 
+print(string.count("geeks", 0, 5)) 
+  
+print(string.count("geeks", 0, 15)) 
